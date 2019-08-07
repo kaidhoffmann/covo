8e81869c117496e9f862f962e0abcc8ab5a025c3
Kai Hoffmann <flediak@gmail.com>
2019-08-07 18:26:17 +0200

    added healpix files and modified makefile accordingly

---
+++ src/healpix.cpp
@@ -0,0 +1,114 @@
+//from https://healpix.jpl.nasa.gov/html/Healpix_cxx/healpix__base2_8cc-source.html
+
+#include "healpix.h"
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+short healpix::utab[];
+
+healpix::Tablefiller::Tablefiller()
+  {
+  for (int m=0; m<0x100; ++m)
+    {
+    utab[m] =
+         (m&0x1 )       | ((m&0x2 ) << 1) | ((m&0x4 ) << 2) | ((m&0x8 ) << 3)
+      | ((m&0x10) << 4) | ((m&0x20) << 5) | ((m&0x40) << 6) | ((m&0x80) << 7);
+    }
+  }
+
+healpix::Tablefiller healpix::Filler;
+
+inline double healpix::fmodulo (double v1, double v2){
+    using namespace std;
+    if (v1>=0)
+    return (v1<v2) ? v1 : fmod(v1,v2);
+    double tmp=fmod(v1,v2)+v2;
+    return (tmp==v2) ? 0. : tmp;
+}
+
+
+int healpix::nside2order (int nside)
+  {
+  if(nside<1) std::cout<< "invalid value for Nside" <<std::endl;
+  if ((nside)&(nside-1)) return -1;
+  return ilog2(nside);
+}
+
+
+int healpix::xyf2nest (int ix, int iy, int face_num, int order) const{
+  return (face_num<<(2*order)) +
+      (utab[ix&0xff] | (utab[ix>>8]<<16)
+    | (utab[iy&0xff]<<1) | (utab[iy>>8]<<17));
+}
+
+
+
+int healpix::ang2pix_z_phi_nest (double z, double phi, int nside) {
+//Nested
+    
+    double za = fabs(z);
+    double tt = fmodulo(phi,twopi) * inv_halfpi; // in [0,4)
+
+    int order = nside2order(nside);
+    
+    int npface = nside*nside;
+
+    int ncap  = (npface-nside)<<1;
+    int npix  = 12*npface;
+
+    int face_num, ix, iy;
+    if (za<=twothird) // Equatorial region
+    {
+
+        double temp1 = nside*(0.5+tt);
+        double temp2 = nside*(z*0.75);
+        int jp = int(temp1-temp2); // index of  ascending edge line
+        int jm = int(temp1+temp2); // index of descending edge line
+        int ifp = jp >> order;  // in {0,4}
+        int ifm = jm >> order;
+        if (ifp == ifm)           // faces 4 to 7
+            face_num = (ifp==4) ? 4: ifp+4;
+        else if (ifp < ifm)       // (half-)faces 0 to 3
+            face_num = ifp;
+        else                      // (half-)faces 8 to 11
+            face_num = ifm + 8;
+
+        ix = jm & (nside-1);
+        iy = nside - (jp & (nside-1)) - 1;
+    }
+    else // polar region, za > 2/3
+        {
+            
+        //std::cout<<"polar region "<<z<<"\t"<<za<<"\t"<<phi<<"\t"<<twothird<<std::endl;
+
+        int ntt = int(tt);
+        if (ntt>=4) ntt=3;
+        double tp = tt-ntt;
+        double tmp = nside*sqrt(3*(1-za));
+
+        int jp = int(tp*tmp); // increasing edge line index
+        int jm = int((1.0-tp)*tmp); // decreasing edge line index
+        if (jp>=nside) jp = nside-1; // for points too close to the boundary
+        if (jm>=nside) jm = nside-1;
+        if (z >= 0)
+            {
+            face_num = ntt;  // in {0,3}
+            ix = nside - jm - 1;
+            iy = nside - jp - 1;
+            }
+        else
+            {
+            face_num = ntt + 8; // in {8,11}
+            ix =  jp;
+            iy =  jm;
+            }
+        }
+
+    return xyf2nest(ix,iy,face_num, order);
+}
+
+    
+int healpix::ang2pix_nest(double theta, double phi, int nside){
+    return ang2pix_z_phi_nest (cos(theta), phi, nside);
+}
---
+++ src/healpix.h
@@ -0,0 +1,92 @@
+/*
+ *  This file was copied and modified from healpix_base.h in Healpix_cxx.
+ *  Copyright (C) 2003, 2004, 2005, 2006 Max-Planck-Society
+ *  (see https://healpix.jpl.nasa.gov/html/Healpix_cxx/healpix__base2_8cc-source.html)
+ *  the original author is Martin Reinecke
+*/
+
+
+#ifndef HEALPIX_BASE_H
+#define HEALPIX_BASE_H
+
+#include <cmath>
+
+
+class healpix{
+    
+    const double pi=3.141592653589793238462643383279502884197;
+    const double twopi=6.283185307179586476925286766559005768394;
+    const double inv_twopi=1.0/twopi;
+    const double fourpi=12.56637061435917295385057353311801153679;
+    const double halfpi=1.570796326794896619231321691639751442099;
+    const double inv_halfpi=0.6366197723675813430755350534900574;
+    const double inv_sqrt4pi = 0.2820947917738781434740397257803862929220;
+
+    const double ln2 = 0.6931471805599453094172321214581766;
+    const double inv_ln2 = 1.4426950408889634073599246810018921;
+    const double ln10 = 2.3025850929940456840179914546843642;
+
+    const double onethird=1.0/3.0;
+    const double twothird=2.0/3.0;
+    const double fourthird=4.0/3.0;
+
+    const double degr2rad=pi/180.0;
+    const double rad2degr=180.0/pi;
+    
+    static short utab[0x100];
+    
+    int xyf2nest (int ix, int iy, int face_num, int order) const;
+    
+    class Tablefiller{
+      public:
+        Tablefiller();
+      };
+    static Tablefiller Filler;
+    
+    /*! Returns the remainder of the division v1/v2.
+    The result is non-negative.
+    v1 can be positive or negative; v2 must be positive. */
+    inline double fmodulo (double v1, double v2);
+    
+    
+    /*! Returns the remainder of the division v1/v2.
+    The result is non-negative.
+    v1 can be positive or negative; \a v2 must be positive. */
+    template<typename I> inline I imodulo (I v1, I v2)
+    { I v=v1%v2; return (v>=0) ? v : v+v2; }
+   
+   
+    /*! Returns the largest integer \a n that fulfills \a 2^n<=arg. */
+    template<typename I> inline int ilog2 (I arg)
+    {
+        #ifdef __GNUC__
+        if (arg==0) return 0;
+        if (sizeof(I)==sizeof(int))
+            return 8*sizeof(int)-1-__builtin_clz(arg);
+        if (sizeof(I)==sizeof(long))
+            return 8*sizeof(long)-1-__builtin_clzl(arg);
+        if (sizeof(I)==sizeof(long long))
+            return 8*sizeof(long long)-1-__builtin_clzll(arg);
+        #endif
+        int res=0;
+        while (arg > 0xFFFF) { res+=16; arg>>=16; }
+        if (arg > 0x00FF) { res|=8; arg>>=8; }
+        if (arg > 0x000F) { res|=4; arg>>=4; }
+        if (arg > 0x0003) { res|=2; arg>>=2; }
+        if (arg > 0x0001) { res|=1; }
+    return res;
+    }
+   
+   int xyf2nest(int ix, int iy, int face_num);
+   
+public:
+    
+    int nside2order (int nside);
+
+    int ang2pix_z_phi_nest (double z, double phi, int nside);
+
+    int ang2pix_nest (double theta, double phi, int nside);
+
+};
+
+#endif
